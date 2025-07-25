10ca25c0bb3df932be2b352438a1800ab461cb7c
Syed Azlan <120971357+syedazlan833@users.noreply.github.com>
2025-07-26 03:10:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
-let main_input = document.getElementById("search__input")
-let main = ()=>{
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=24.860966&lon=66.990501&q=${search__input.value}&appid=1502934fbaa78da2c7d642461ba15103&units=metric`)
-    .then((res) => res.json())
-    .then((res) => {
-        let get = document.getElementById("degree-celcius")
-        get.innerHTML =Math.round( res.main.temp) + "°C"
-        let wind = document.getElementById("wind")
-        wind.innerHTML = res.wind.speed +"km/h"
-        let description = document.getElementById("description")
-        description.innerHTML = res.weather[0].main
-        let humidity = document.getElementById("humidity")
-        humidity.innerHTML = res.main.humidity + "%"
-        let country = document.getElementById("country")
-        country.innerHTML = res.name + ", " + res.sys.country
-        let whether_images = document.getElementById("wheather-main-inner-image")
-        if(res.weather[0].main === `Clouds`){
-            whether_images.src = "https://cdn-icons-png.flaticon.com/512/4834/4834559.png"
-        }
-        else if(res.weather[0].main === `Mist`){
-            whether_images.src = "https://cdn-icons-png.flaticon.com/512/4005/4005901.png"
-        }
-        else if(res.weather[0].main === `Fog`){
-            whether_images.src = "https://w7.pngwing.com/pngs/244/421/png-transparent-weather-clouds-fog-foggy-weather-color-icon.png"
-        }
-        else if(res.weather[0].main === `Smoke`){
-            whether_images.src = "https://cdn-icons-png.flaticon.com/512/3540/3540959.png"
-        }
-        else if(res.weather[0].main === `Clear`){
-            whether_images.src = "https://cdn-icons-png.flaticon.com/512/3222/3222800.png"
-        }
-        else if(res.weather[0].main === `Haze`){
-            whether_images.src = "https://www.nicepng.com/png/detail/123-1236627_haze-icon-png-haze-weather-icon.png"
-        }
-        else if(res.weather[0].main === `Rain`){
-            whether_images.src = "https://files.softicons.com/download/web-icons/vector-stylish-weather-icons-by-bartosz-kaszubowski/png/256x256/cloud.rain.png"
-        }
-        else{
-            whether_images.src = "https://w7.pngwing.com/pngs/166/384/png-transparent-cloud-sun-sunny-weather-weather-flat-icon.png"
-        }
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+const getWeather = () => {
+    const input = document.getElementById("search__input").value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=1502934fbaa78da2c7d642461ba15103&units=metric`)
+        .then(res => res.json())
+        .then(res => {
+            document.getElementById("degree-celcius").textContent = Math.round(res.main.temp) + "°C";
+            document.getElementById("wind").textContent = res.wind.speed + " km/h";
+            document.getElementById("description").textContent = res.weather[0].main;
+            document.getElementById("humidity").textContent = res.main.humidity + "%";
+            document.getElementById("country").textContent = res.name + ", " + res.sys.country;
+
+            const iconMap = {
+                Clouds: "https://cdn-icons-png.flaticon.com/512/4834/4834559.png",
+                Mist: "https://cdn-icons-png.flaticon.com/512/4005/4005901.png",
+                Fog: "https://w7.pngwing.com/pngs/244/421/png-transparent-weather-clouds-fog-foggy-weather-color-icon.png",
+                Smoke: "https://static.vecteezy.com/system/resources/previews/024/729/179/original/night-strong-wind-icon-weather-forecast-sign-png.png",
+                Clear: "https://cdn-icons-png.flaticon.com/512/3222/3222800.png",
+                Haze: "https://cdn-icons-png.flaticon.com/512/1779/1779807.png",
+                Rain: "https://cdn-icons-png.flaticon.com/512/4724/4724091.png"
+            };
+
+            document.getElementById("weather-icon").src = iconMap[res.weather[0].main] || "https://w7.pngwing.com/pngs/166/384/png-transparent-cloud-sun-sunny-weather-weather-flat-icon.png";
+        })
+        .catch(err => console.log(err));
+    };
+
+const backgrounds = [
+    "https://images.unsplash.com/photo-1506744038136-46273834b3fb", // Mountains, default
+    "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8", // Sunrise
+    "https://images.unsplash.com/photo-1448375240586-882707db888b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  // Waterfall
+];
+
+let bgIndex = 0;
+function toggleBackground() {
+    bgIndex = (bgIndex + 1) % backgrounds.length;
+    document.body.style.backgroundImage = `url('${backgrounds[bgIndex]}')`;
 }
